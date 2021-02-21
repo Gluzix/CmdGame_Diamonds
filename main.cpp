b625dcfaf2228389a8fb94b5b0535e69df163784
Kamil <kamil.gluza@gmail.com>
2021-02-21 19:08:28 +0100

    Added move possibility for the player

--- main.cpp
+++ main.cpp
@@ -78,6 +78,7 @@ int main()
     m1.create_menu();
     Map mp1(check_width("resources/Map.txt"), check_height("resources/Map.txt"), "resources/Map.txt");
     mp1.create_map();
+    int way = 0;
 
     while(1)
     {
@@ -107,11 +108,12 @@ int main()
                 mp1.show_map();
 
                 std::vector<Enemy*> container;
+                player player(mp1.return_player_pos_x(), mp1.return_player_pos_y());
 
-                //for (int i = 0; i < 14; i++)
-                //{
-                //    container.push_back(SlowEnemy(mp1.return_slow_enem_pos_x(i), mp1.return_slow_enem_pos_y(i)));
-                //}
+                for (int i = 0; i < 13; i++)
+                {
+                    container.push_back(new SlowEnemy(mp1.return_slow_enem_pos_x(i), mp1.return_slow_enem_pos_y(i)));
+                }
 
                 for (int i = 0; i < 5; i++)
                 {
@@ -120,54 +122,140 @@ int main()
 
                 while(1)
                 {
-                    for (int i = 0; i < 5; i++)
+                    if (GetAsyncKeyState(VK_UP))
                     {
-                        ((FastEnemy*)container[i])->UpdatePosition();
+                        way = 0;
+                    }
+                    else if (GetAsyncKeyState(VK_DOWN))
+                    {
+                        way = 1;
+                    }
+                    else if (GetAsyncKeyState(VK_LEFT))
+                    {
+                        way = 2;
+                    }
+                    else if (GetAsyncKeyState(VK_RIGHT))
+                    {
+                        way = 3;
+                    }
+
+                    player.try_to_move(way);
+                    if (!mp1.check_if_obstacle_for_player(player.return_pos_x(), player.return_pos_y()))
+                    {
+                        player.restore_coords();
+                    }
+                    player.update_plyer();
+                    way = -1;
+
+                    for (int i = 13; i < 17; i++)
+                    {   
+                        if (!((FastEnemy*)container[i]->IsPlayerNear(player.return_pos_x(), player.return_pos_y())))
+                        {
+                            ((FastEnemy*)container[i])->UpdatePosition();
+                        }
+                       
                         while (!mp1.check_if_obstacle_for_fast_enem(((FastEnemy*)container[i])->GetX(), ((FastEnemy*)container[i])->GetY(), i) ||
                             !mp1.check_if_faster_enem_meet_enem(((FastEnemy*)container[i])->GetX(), ((FastEnemy*)container[i])->GetY(), i))
                         {
                             ((FastEnemy*)container[i])->BackToOldPosition();
+                            if (!((FastEnemy*)container[i]->IsPlayerNear(player.return_pos_x(), player.return_pos_y())))
+                            {
+                                ((FastEnemy*)container[i])->UpdatePosition();
+                            }
+                        }     
+                        ((FastEnemy*)container[i])->UpdateConsoleCoordinates();
+                    }
+                    Sleep(100);
+
+                    if (GetAsyncKeyState(VK_UP))
+                    {
+                        way = 0;
+                    }
+                    else if (GetAsyncKeyState(VK_DOWN))
+                    {
+                        way = 1;
+                    }
+                    else if (GetAsyncKeyState(VK_LEFT))
+                    {
+                        way = 2;
+                    }
+                    else if (GetAsyncKeyState(VK_RIGHT))
+                    {
+                        way = 3;
+                    }
+
+                    player.try_to_move(way);
+                    if (!mp1.check_if_obstacle_for_player(player.return_pos_x(), player.return_pos_y()))
+                    {
+                        player.restore_coords();
+                    }
+                    player.update_plyer();
+                    way = -1;
+
+                    for (int i = 13; i < 17; i++)
+                    {
+                        if (!((FastEnemy*)container[i]->IsPlayerNear(player.return_pos_x(), player.return_pos_y())))
+                        {
                             ((FastEnemy*)container[i])->UpdatePosition();
+                        }
+
+                        while (!mp1.check_if_obstacle_for_fast_enem(((FastEnemy*)container[i])->GetX(), ((FastEnemy*)container[i])->GetY(), i) ||
+                            !mp1.check_if_faster_enem_meet_enem(((FastEnemy*)container[i])->GetX(), ((FastEnemy*)container[i])->GetY(), i))
+                        {
+                            ((FastEnemy*)container[i])->BackToOldPosition();
+                            if (!((FastEnemy*)container[i]->IsPlayerNear(player.return_pos_x(), player.return_pos_y())))
+                            {
+                                ((FastEnemy*)container[i])->UpdatePosition();
+                            }
                         }
                         ((FastEnemy*)container[i])->UpdateConsoleCoordinates();
                     }
-                    Sleep(3);
-                    //Sleep(1);
-                    //for (int i = 14; i < 20; i++)
-                    //{
-                    //    container[i].UpdatePosition();
-                    //    while (!mp1.check_if_obstacle_for_fast_enem(container[i].GetX(), container[i].GetY(), i) ||
-                    //        !mp1.check_if_faster_enem_meet_enem(container[i].GetX(), container[i].GetY(), i))
-                    //    {
-                    //        container[i].BackToOldPosition();
-                    //        container[i].UpdatePosition();
-                    //    }
-                    //    container[i].UpdateConsoleCoordinates();
-                    //}
-                    //Sleep(1);
-                    //for (int i = 14; i < 20; i++)
-                    //{
-                    //    container[i].UpdatePosition();
-                    //    while (!mp1.check_if_obstacle_for_fast_enem(container[i].GetX(), container[i].GetY(), i) ||
-                    //        !mp1.check_if_faster_enem_meet_enem(container[i].GetX(), container[i].GetY(), i))
-                    //    {
-                    //        container[i].BackToOldPosition();
-                    //        container[i].UpdatePosition();
-                    //    }
-                    //    container[i].UpdateConsoleCoordinates();
-                    //}
-                    //Sleep(1);
-                    //for (int i = 0; i < 14; i++)
-                    //{
-                    //    container[i].UpdatePosition();
-                    //    while (!mp1.check_if_obstacle_for_slow_enem(container[i].GetX(), container[i].GetY(), i) ||
-                    //        !mp1.check_if_slow_enem_meet_enem(container[i].GetX(), container[i].GetY(), i))
-                    //    {
-                    //        container[i].BackToOldPosition();
-                    //        container[i].UpdatePosition();
-                    //    }
-                    //    container[i].UpdateConsoleCoordinates();
-                    //}
+                    Sleep(100);
+
+                    if (GetAsyncKeyState(VK_UP))
+                    {
+                        way = 0;
+                    }
+                    else if (GetAsyncKeyState(VK_DOWN))
+                    {
+                        way = 1;
+                    }
+                    else if (GetAsyncKeyState(VK_LEFT))
+                    {
+                        way = 2;
+                    }
+                    else if (GetAsyncKeyState(VK_RIGHT))
+                    {
+                        way = 3;
+                    }
+
+                    player.try_to_move(way);
+                    if (!mp1.check_if_obstacle_for_player(player.return_pos_x(), player.return_pos_y()))
+                    {
+                        player.restore_coords();
+                    }
+                    player.update_plyer();
+                    way = -1;
+
+                    for (int i = 0; i < 13; i++)
+                    {
+                        if (!((SlowEnemy*)container[i]->IsPlayerNear(player.return_pos_x(), player.return_pos_y())))
+                        {
+                            ((SlowEnemy*)container[i])->UpdatePosition();
+                        }
+
+                        while (!mp1.check_if_obstacle_for_slow_enem(((SlowEnemy*)container[i])->GetX(), ((SlowEnemy*)container[i])->GetY(), i) ||
+                            !mp1.check_if_slow_enem_meet_enem(((SlowEnemy*)container[i])->GetX(), ((SlowEnemy*)container[i])->GetY(), i))
+                        {
+                            ((SlowEnemy*)container[i])->BackToOldPosition();
+                            if (!((SlowEnemy*)container[i]->IsPlayerNear(player.return_pos_x(), player.return_pos_y())))
+                            {
+                                ((SlowEnemy*)container[i])->UpdatePosition();
+                            }
+                        }
+                        ((SlowEnemy*)container[i])->UpdateConsoleCoordinates();
+
+                    }
                 }
             }
             else if(x==1)
