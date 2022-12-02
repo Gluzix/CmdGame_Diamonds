df9d5a669df112ee0b31b5741be3f4afa3d9cb51
Ka Mil <kamil.gluza@gmail.com>
2022-12-02 22:50:11 +0100

    WIP - improving player

--- header/Player.h
+++ header/Player.h
@@ -1,16 +1,19 @@
-#ifndef PLAYER_H
-#define PLAYER_H
-class player
-{
-    int pos_x, pos_y, old_pos_x, old_pos_y;
-    public:
-        player(int x ,int y): pos_x(x), pos_y(y), old_pos_x(x), old_pos_y(y){}
-        ~player(){}
-        void try_to_move(int);
-        void update_plyer();
-        void restore_coords();
-        int return_pos_x();
-        int return_pos_y();
-};
+#pragma once
 
-#endif // PLAYER_H
+#include <Coordinates.h>
+
+class Player
+{
+public:
+    Player(const Coordinates &coords);
+
+    void try_to_move(int);
+    void update_plyer();
+    void restore_coords();
+    int return_pos_x();
+    int return_pos_y();
+
+private:
+    Coordinates coordinates;
+    Coordinates oldCoordinates;
+};
