ca442d84870ab13c7cd6499e0ec63899e5071740
Ka Mil <kamil.gluza@gmail.com>
2022-12-02 22:50:11 +0100

    WIP - improving player

--- source/Player.cpp
+++ source/Player.cpp
@@ -1,10 +1,16 @@
-#include "player.h"
+#include "Player.h"
 #include <iostream>
 #include <windows.h>
 #include <cstdlib>
-using namespace std;
 
-void player::try_to_move(int try_way)
+Player::Player(const Coordinates& coords)
+    : coordinates(coords)
+    , oldCoordinates(coords)
+{
+
+}
+
+void Player::try_to_move(int try_way)
 {
     switch(try_way)
     {
@@ -22,32 +28,36 @@ void player::try_to_move(int try_way)
             break;
     }
 }
-int player::return_pos_x()
+
+int Player::return_pos_x()
 {
     return pos_x;
 }
-int player::return_pos_y()
+
+int Player::return_pos_y()
 {
     return pos_y;
 }
-void player::restore_coords()
+
+void Player::restore_coords()
 {
     pos_x=old_pos_x;
     pos_y=old_pos_y;
 }
-void player::update_plyer()
+
+void Player::update_plyer()
 {
-        const char *buff=" ";
-        const char *enem="@";
-        HANDLE hOutput = GetStdHandle(STD_OUTPUT_HANDLE);
-        COORD coord_first = {pos_x,pos_y};
-        COORD coord_second = {old_pos_x,old_pos_y};
-        SetConsoleCursorPosition(hOutput, coord_second);
-        WriteConsoleA(hOutput, buff, 1, NULL,NULL);
-        SetConsoleTextAttribute(hOutput, FOREGROUND_INTENSITY | FOREGROUND_GREEN );
-        SetConsoleCursorPosition(hOutput, coord_first);
-        WriteConsoleA(hOutput, enem, 1, NULL,NULL);
-        SetConsoleTextAttribute ( hOutput, FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
-        old_pos_x=pos_x;
-        old_pos_y=pos_y;
+    const char *buff=" ";
+    const char *enem="@";
+    HANDLE hOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD coord_first = {pos_x,pos_y};
+    COORD coord_second = {old_pos_x,old_pos_y};
+    SetConsoleCursorPosition(hOutput, coord_second);
+    WriteConsoleA(hOutput, buff, 1, NULL,NULL);
+    SetConsoleTextAttribute(hOutput, FOREGROUND_INTENSITY | FOREGROUND_GREEN );
+    SetConsoleCursorPosition(hOutput, coord_first);
+    WriteConsoleA(hOutput, enem, 1, NULL,NULL);
+    SetConsoleTextAttribute ( hOutput, FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+    old_pos_x=pos_x;
+    old_pos_y=pos_y;
 }
